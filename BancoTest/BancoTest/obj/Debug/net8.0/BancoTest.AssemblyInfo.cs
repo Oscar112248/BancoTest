75ec5de31402c9a199e5fb818cf7ea156f013f26
Oscar <omaldonado@dmq.quitodm.inst>
2025-08-30 21:22:26 -0500

    BC TEST

--- BancoTest/BancoTest/obj/Debug/net8.0/BancoTest.AssemblyInfo.cs
+++ BancoTest/BancoTest/obj/Debug/net8.0/BancoTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BancoTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4903acc7ff40e953aa389be235104669bb740b14")]
 [assembly: System.Reflection.AssemblyProductAttribute("BancoTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BancoTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
